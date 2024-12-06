e72a717ad88066d1d0507ca4af7a43e673927bb2
Erick Urquilla <erickurquilla1999@gmail.com>
2024-12-06 06:33:00 -0800

    Setting latex to false

--- nsm_plots/nsm_movies.py
+++ nsm_plots/nsm_movies.py
@@ -23,7 +23,7 @@ import glob
 # Font settings
 mpl.rcParams['font.size'] = 22
 mpl.rcParams['font.family'] = 'serif'
-mpl.rc('text', usetex=True)
+mpl.rc('text', usetex=False)
 
 # Tick settings
 mpl.rcParams['xtick.major.width'] = 2
