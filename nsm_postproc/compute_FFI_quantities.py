7600e717942c0e0abf845bc6acf55799cc83fd66
erickurquilla1999 <erickurquilla1999@gmail.com>
2025-05-29 17:38:39 -0400

    Compute FFI quantities just for slices

--- nsm_postproc/compute_FFI_quantities.py
+++ nsm_postproc/compute_FFI_quantities.py
@@ -15,9 +15,32 @@ x_cell_ind = np.array([int(file_name.split('_')[1]) for file_name in cell_file_n
 y_cell_ind = np.array([int(file_name.split('_')[2]) for file_name in cell_file_names])
 z_cell_ind = np.array([int((file_name.split('_')[3]).split('.')[0]) for file_name in cell_file_names])
 cell_indices = np.array(list(zip(x_cell_ind, y_cell_ind, z_cell_ind)))
+
+# Get the cell indices for ix fix but iy and iz varying all available cells
+x_idx_slice = 48
+mask_yz_slice = cell_indices[:,0] == x_idx_slice # fixing the x index in this value
+cell_indices_yz_slice = cell_indices[mask_yz_slice]
+
+# Get the cell indices for iy fix but ix and iz varying all available cells
+y_idx_slice = 48
+mask_xz_slice = cell_indices[:,1] == y_idx_slice # fixing the y index in this value
+cell_indices_xz_slice = cell_indices[mask_xz_slice]
+
+# Get the cell indices for iz fix but ix and iy varying all available cells
+z_idx_slice = 16
+mask_xy_slice = cell_indices[:,2] == z_idx_slice # fixing the z index in this value
+cell_indices_xy_slice = cell_indices[mask_xy_slice]
+
+# Combine all cell indices from the three slices
+# cell_indices_all = np.concatenate((cell_indices_yz_slice, cell_indices_xz_slice, cell_indices_xy_slice), axis=0)
+cell_indices_all = np.concatenate((cell_indices_xz_slice, cell_indices_xy_slice), axis=0)
+cell_indices_all = np.unique(cell_indices_all, axis=0)
+
+cell_indices = cell_indices_all
+
 print('Number of cells:', len(cell_indices))
 
-cellvolume = 1.0  # ccm
+cellvolume = 1.0e5**3 # ccm
 
 def writehdf5file(indexpair):
     i, j, k = indexpair
