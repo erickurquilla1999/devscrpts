961f07a73e4d5ac8ee7a520723c93f9f20eb2c1a
erickurquilla1999 <erickurquilla1999@gmail.com>
2025-06-15 18:24:49 -0400

    Solving issue with integral over solid angle wrongly implemented

--- nsm_instabilities/FFI_functions.py
+++ nsm_instabilities/FFI_functions.py
@@ -61,8 +61,9 @@ def compute_sigma_GnPos_GnNeg(i, j, k, directory, cellvolume):
         mask_pos_eln = G_ELN > 0
         mask_neg_eln = G_ELN < 0
 
-        GnPos = +1.0 * ( domega / (4 * np.pi) ) * np.sum(G_ELN[mask_pos_eln])
-        GnNeg = -1.0 * ( domega / (4 * np.pi) ) * np.sum(G_ELN[mask_neg_eln])
+        GnPos = +1.0 * np.sum(G_ELN[mask_pos_eln])
+        GnNeg = -1.0 * np.sum(G_ELN[mask_neg_eln])
+
         sigma = np.sqrt(GnPos*GnNeg) / (2 * np.pi)
 
         print(f'sigma={sigma:.3e} 1/s, GnPos={GnPos:.3e} 1/s, GnNeg={GnNeg:.3e} 1/s, i={i}, j={j}, k={k}')
